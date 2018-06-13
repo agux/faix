44c7d0173076f0b863e191295ba694bf1e02c4fb
Jonathan <carusyte@163.com>
2018-06-14 00:09:11 +0800

    create SRnnRegressorV3

--- corl/model/base.py
+++ corl/model/base.py
@@ -358,3 +358,103 @@ class SRnnRegressorV2:
             predict = tf.gather(logits, bidx)
             actual = tf.gather(self.target, bidx)
             return bidx, max_diff, predict, actual
+
+class SRnnRegressorV3:
+    '''
+    Simple RNN Regressor using GridRNNCell, internal cell type is LSTMBlockCell.
+    '''
+
+    def __init__(self, data, target, seqlen, layer_width=200, dim=3, learning_rate=1e-3):
+        self.data = data
+        self.target = target
+        self.seqlen = seqlen
+        self._layer_width = layer_width
+        self._dim = dim
+        self._learning_rate = learning_rate
+        self.logits
+        self.optimize
+        self.cost
+        self.worst
+
+    def getName(self):
+        return self.__class__.__name__
+
+    @lazy_property
+    def logits(self):
+        layer = self.rnn(self, self.data)
+        with tf.variable_scope("output"):
+            layer = tf.nn.selu(layer)
+            output = tf.layers.dense(
+                inputs=layer,
+                units=1,
+                kernel_initializer=tf.variance_scaling_initializer(),
+                bias_initializer=tf.constant_initializer(0.1)
+            )
+            output = tf.squeeze(output)
+            return output
+
+    @staticmethod
+    def newCell(width, _dim):
+        def cell_fn(n):
+            return tf.contrib.rnn.LSTMBlockCell(
+                num_units=n,
+                use_peephole=True
+            )
+        c = tf.contrib.grid_rnn.GridRNNCell(
+            num_units=width,
+            num_dims=_dim,
+            input_dims=0,
+            output_dims=0,
+            priority_dims=0,
+            tied=False,
+            non_recurrent_dims=None,
+            cell_fn=cell_fn,
+            non_recurrent_fn=None,
+            state_is_tuple=True,
+            output_is_tuple=True
+        )
+        return c
+
+    @staticmethod
+    def rnn(self, inputs):
+        output, _ = tf.nn.dynamic_rnn(
+            self.newCell(self._layer_width, self._dim),
+            inputs,
+            dtype=tf.float32,
+            sequence_length=self.seqlen
+        )
+        output = tf.concat(output, 1)
+        output = self.last_relevant(output, self.seqlen)
+        print('last time step: {}'.format(output.get_shape()))
+        return output
+
+    @staticmethod
+    def last_relevant(output, length):
+        with tf.name_scope("last_relevant"):
+            batch_size = tf.shape(output)[0]
+            relevant = tf.gather_nd(output, tf.stack(
+                [tf.range(batch_size), length-1], axis=1))
+            return relevant
+
+    @lazy_property
+    def cost(self):
+        logits = self.logits
+        with tf.name_scope("cost"):
+            return tf.losses.mean_squared_error(labels=self.target, predictions=logits)
+
+    @lazy_property
+    def optimize(self):
+        return tf.train.AdamOptimizer(self._learning_rate,
+                                      epsilon=1e-7).minimize(
+            self.cost, global_step=tf.train.get_global_step())
+
+    @lazy_property
+    def worst(self):
+        logits = self.logits
+        with tf.name_scope("worst"):
+            sqd = tf.squared_difference(logits, self.target)
+            bidx = tf.argmax(sqd)
+            max_diff = tf.sqrt(tf.reduce_max(sqd))
+            predict = tf.gather(logits, bidx)
+            actual = tf.gather(self.target, bidx)
+            return bidx, max_diff, predict, actual
