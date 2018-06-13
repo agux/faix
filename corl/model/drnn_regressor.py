87e2b26d0f300e32c7ea71ad14a18f7e5773abbf
Jonathan <carusyte@163.com>
2018-06-13 23:14:12 +0800

    submit test3

--- corl/model/drnn_regressor.py
+++ corl/model/drnn_regressor.py
@@ -10,9 +10,11 @@ import math
 from pstk.model.model import lazy_property, stddev
 
 # pylint: disable-msg=E1101
+
+
 class DRnnRegressorV1:
     '''
-    Simple RNN Regressor using one of:
+    Deep RNN Regressor using one of:
     GRU,
     GRUBlock,
     LSTM,
@@ -161,7 +163,7 @@ class DRnnRegressorV1:
         cells = []
         _cell = self._cell.lower()
         for _ in range(2):
-            cells.append(self.newCell(self,_cell))
+            cells.append(self.newCell(self, _cell))
         mc = tf.nn.rnn_cell.MultiRNNCell(cells)
         output, _ = tf.nn.dynamic_rnn(
             mc,
@@ -169,6 +171,123 @@ class DRnnRegressorV1:
             dtype=tf.float32,
             sequence_length=self.seqlen
         )
+        output = self.last_relevant(output, self.seqlen)
+        print('last time step: {}'.format(output.get_shape()))
+        return output
+
+    @staticmethod
+    def last_relevant(output, length):
+        with tf.name_scope("last_relevant"):
+            batch_size = tf.shape(output)[0]
+            relevant = tf.gather_nd(output, tf.stack(
+                [tf.range(batch_size), length-1], axis=1))
+            return relevant
+
+    @lazy_property
+    def cost(self):
+        logits = self.logits
+        with tf.name_scope("cost"):
+            return tf.losses.mean_squared_error(labels=self.target, predictions=logits)
+
+    @lazy_property
+    def optimize(self):
+        return tf.train.AdamOptimizer(self._learning_rate,
+                                      epsilon=1e-7).minimize(
+            self.cost, global_step=tf.train.get_global_step())
+
+    @lazy_property
+    def worst(self):
+        logits = self.logits
+        with tf.name_scope("worst"):
+            sqd = tf.squared_difference(logits, self.target)
+            bidx = tf.argmax(sqd)
+            max_diff = tf.sqrt(tf.reduce_max(sqd))
+            predict = tf.gather(logits, bidx)
+            actual = tf.gather(self.target, bidx)
+            return bidx, max_diff, predict, actual
+
+
+class DRnnRegressorV2:
+    '''
+    Deep RNN Regressor using GridRNNCell, internal cell type is LSTMBlockCell.
+    '''
+
+    def __init__(self, data, target, seqlen, layer_width=200, dim=3, learning_rate=1e-3):
+        self.data = data
+        self.target = target
+        self.seqlen = seqlen
+        self._layer_width = layer_width
+        self._dim = dim
+        self._learning_rate = learning_rate
+        self.logits
+        self.optimize
+        self.cost
+        self.worst
+
+    def getName(self):
+        return self.__class__.__name__
+
+    @lazy_property
+    def logits(self):
+        layer = self.rnn(self, self.data)
+        layer = self.fcn(self, layer)
+        with tf.variable_scope("output"):
+            output = tf.layers.dense(
+                inputs=layer,
+                units=1,
+                kernel_initializer=tf.variance_scaling_initializer(),
+                bias_initializer=tf.constant_initializer(0.1)
+            )
+            output = tf.squeeze(output)
+            return output
+
+    @staticmethod
+    def fcn(self, inputs):
+        layer = inputs
+        with tf.variable_scope("fcn"):
+            layer = tf.nn.selu(layer)
+            for _ in range(2):
+                width = int(layer.get_shape()[-1])//2
+                layer = tf.layers.dense(
+                    inputs=layer,
+                    units=width,
+                    kernel_initializer=tf.variance_scaling_initializer(),
+                    bias_initializer=tf.constant_initializer(0.1),
+                    activation=tf.nn.selu
+                )
+        return layer
+
+    @staticmethod
+    def newCell(width, _dim):
+        def cell_fn(n):
+            return tf.contrib.rnn.LSTMBlockCell(
+                num_units=n,
+                use_peephole=True
+            )
+        c = tf.contrib.grid_rnn.GridRNNCell(
+            num_units=width,
+            num_dims=_dim,
+            input_dims=0,
+            output_dims=0,
+            priority_dims=0,
+            tied=False,
+            non_recurrent_dims=None,
+            cell_fn=cell_fn,
+            non_recurrent_fn=None,
+            state_is_tuple=True,
+            output_is_tuple=True
+        )
+        return c
+
+    @staticmethod
+    def rnn(self, inputs):
+        output, _ = tf.nn.dynamic_rnn(
+            self.newCell(self._layer_width, self._dim),
+            inputs,
+            dtype=tf.float32,
+            sequence_length=self.seqlen
+        )
+        output = tf.concat(output, 1)
         output = self.last_relevant(output, self.seqlen)
         print('last time step: {}'.format(output.get_shape()))
         return output
---
+++ corl/wc_test/test3.py
@@ -0,0 +1,126 @@
+from __future__ import print_function
+# Path hack.
+import sys
+import os
+sys.path.append(os.path.dirname(os.path.abspath(__file__)) + "/..")
+
+import tensorflow as tf
+# pylint: disable-msg=E0401
+from model import drnn_regressor
+from wc_data import base as data0
+from test1 import collect_summary
+from time import strftime
+import os
+import numpy as np
+import math
+
+N_TEST = 100
+TEST_INTERVAL = 50
+LAYER_WIDTH = 512
+MAX_STEP = 50
+TIME_SHIFT = 4
+DIM = 5
+LEARNING_RATE = 1e-3
+LOG_DIR = 'logdir'
+
+# pylint: disable-msg=E0601
+
+
+k_cols = [
+    "lr", "lr_h", "lr_o", "lr_l",
+    "lr_h_c", "lr_o_c", "lr_l_c",
+    "lr_ma5", "lr_ma5_h", "lr_ma5_o", "lr_ma5_l",
+    "lr_ma10", "lr_ma10_h", "lr_ma10_o", "lr_ma10_l",
+    "lr_vol", "lr_vol5", "lr_vol10"
+]
+
+
+def run():
+    tf.logging.set_verbosity(tf.logging.INFO)
+    loader = data0.DataLoader(TIME_SHIFT, k_cols)
+    print('{} loading test data...'.format(strftime("%H:%M:%S")))
+    tuuids, tdata, tvals, tseqlen = loader.loadTestSet(MAX_STEP, N_TEST)
+    print('input shape: {}'.format(tdata.shape))
+    print('target shape: {}'.format(tvals.shape))
+    featSize = tdata.shape[2]
+    data = tf.placeholder(tf.float32, [None, MAX_STEP, featSize], "input")
+    target = tf.placeholder(tf.float32, [None], "target")
+    seqlen = tf.placeholder(tf.int32, [None], "seqlen")
+    with tf.Session() as sess:
+        model = drnn_regressor.DRnnRegressorV2(
+            data=data,
+            target=target,
+            seqlen=seqlen,
+            dim = DIM,
+            layer_width=LAYER_WIDTH,
+            learning_rate=LEARNING_RATE)
+        model_name = model.getName()
+        f = __file__
+        fbase = f[f.rfind('/')+1:f.rindex('.py')]
+        base_dir = '{}/{}_{}/{}'.format(LOG_DIR, fbase,
+                                        model_name, strftime("%Y%m%d_%H%M%S"))
+        print('{} using model: {}'.format(strftime("%H:%M:%S"), model_name))
+        if tf.gfile.Exists(base_dir):
+            tf.gfile.DeleteRecursively(base_dir)
+        tf.gfile.MakeDirs(base_dir)
+        sess.run(tf.global_variables_initializer())
+        summary, train_writer, test_writer = collect_summary(
+            sess, model, base_dir)
+        saver = tf.train.Saver()
+        bno = 0
+        epoch = 0
+        while True:
+            bno = epoch*TEST_INTERVAL
+            print('{} running on test set...'.format(strftime("%H:%M:%S")))
+            feeds = {data: tdata, target: tvals, seqlen: tseqlen}
+            mse, worst, test_summary_str = sess.run(
+                [model.cost, model.worst, summary], feeds)
+            bidx, max_diff, predict, actual = worst[0], worst[1], worst[2], worst[3]
+            print('{} Epoch {} diff {:3.5f} max_diff {:3.4f} predict {} actual {} uuid {}'.format(
+                strftime("%H:%M:%S"), epoch, math.sqrt(mse), max_diff, predict, actual, tuuids[bidx]))
+            summary_str = None
+            fin = False
+            for _ in range(TEST_INTERVAL):
+                bno = bno+1
+                print('{} loading training data for batch {}...'.format(
+                    strftime("%H:%M:%S"), bno))
+                _, trdata, trvals, trseqlen = loader.loadTrainingData(
+                    bno, MAX_STEP)
+                if len(trdata) > 0:
+                    print('{} training...'.format(strftime("%H:%M:%S")))
+                else:
+                    print('{} end of training data, finish training.'.format(
+                        strftime("%H:%M:%S")))
+                    fin = True
+                    break
+                feeds = {data: trdata, target: trvals,  seqlen: trseqlen}
+                summary_str, worst = sess.run(
+                    [summary, model.worst, model.optimize], feeds)[:-1]
+                bidx, max_diff, predict, actual = worst[0], worst[1], worst[2], worst[3]
+                print('{} bno {} max_diff {:3.4f} predict {} actual {}'.format(
+                    strftime("%H:%M:%S"), bno, max_diff, predict, actual))
+                train_writer.add_summary(summary_str, bno)
+                test_writer.add_summary(test_summary_str, bno)
+                train_writer.flush()
+                test_writer.flush()
+            checkpoint_file = os.path.join(base_dir, 'model.ckpt')
+            saver.save(sess, checkpoint_file, global_step=bno)
+            epoch += 1
+            if fin:
+                break
+        # test last epoch
+        print('{} running on test set...'.format(strftime("%H:%M:%S")))
+        feeds = {data: tdata, target: tvals, seqlen: tseqlen}
+        mse, worst, test_summary_str = sess.run(
+            [model.cost, model.worst, summary], feeds)
+        bidx, max_diff, predict, actual = worst[0], worst[1], worst[2], worst[3]
+        print('{} Epoch {} diff {:3.5f} max_diff {:3.4f} predict {} actual {} uuid {}'.format(
+            strftime("%H:%M:%S"), epoch, math.sqrt(mse), max_diff, predict, actual, tuuids[bidx]))
+        train_writer.add_summary(summary_str, bno)
+        test_writer.add_summary(test_summary_str, bno)
+        train_writer.flush()
+        test_writer.flush()
+
+
+if __name__ == '__main__':
+    run()
