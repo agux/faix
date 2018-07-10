4321230f326825ac6a083a815d34ff5dbbca63d2
Jonathan <carusyte@163.com>
2018-07-10 10:16:23 +0800

    added test12 decayed learning rate

--- corl/wc_test/test12.py
+++ corl/wc_test/test12.py
@@ -9,6 +9,7 @@ import tensorflow as tf
 from model import drnn_regressor as drnn
 from wc_data import input_fn, input_bq, input_file
 from time import strftime
+from test11 import validate, bst_saver, bst_score, bst_file, bst_ckpt, parseArgs, feat_cols, LOG_DIR, collect_summary, getInput
 import argparse
 import numpy as np
 import math
@@ -16,7 +17,6 @@ import multiprocessing
 import shutil
 import random
 
-# N_TEST = 100
 VSET = 9
 TEST_BATCH_SIZE = 3000
 TEST_INTERVAL = 50
@@ -24,96 +24,14 @@ SAVE_INTERVAL = 10
 LAYER_WIDTH = 512
 MAX_STEP = 35
 TIME_SHIFT = 4
-DIM = 6
+DIM = 8
 KEEP_PROB = 0.5
 LEARNING_RATE = 1e-3
+DECAYED_LEARNING_RATE = 1e-4
+DECAYED_LR_START = 0.7
 SEED = 285139
-LOG_DIR = 'logdir'
-
-feat_cols = ["lr", "lr_vol"]
 
 # pylint: disable-msg=E0601,E1101
-
-bst_saver, bst_score, bst_file, bst_ckpt = None, None, None, None
-
-
-def parseArgs():
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--ds', type=str, help='datasource. such as file, db, or BigQuery.',
-                        default='db')
-    parser.add_argument('--dir', type=str,
-                        help='directory path for training and test set.')
-    parser.add_argument('--parallel', type=int, help='database operation parallel level',
-                        default=multiprocessing.cpu_count())
-    parser.add_argument('--prefetch', type=int, help='dataset prefetch batches',
-                        default=2)
-    parser.add_argument('--db_host', type=str, help='database host address',
-                        default=None)
-    parser.add_argument('--db_port', type=int, help='database listening port',
-                        default=None)
-    parser.add_argument('--db_pwd', type=str, help='database password',
-                        default=None)
-    parser.add_argument('--vset', type=int, help='validation set number',
-                        default=None)
-    parser.add_argument('--db_pool', type=int, help='database connection pool size',
-                        default=multiprocessing.cpu_count())
-    parser.add_argument('--start', type=int, help='start training at specified batch no',
-                        default=None)
-    parser.add_argument(
-        '--restart', help='restart training', action='store_true')
-    return parser.parse_args()
-
-
-def collect_summary(sess, model, base_dir):
-    ts = strftime("%Y%m%d_%H%M%S")
-    train_writer = tf.summary.FileWriter(os.path.join(
-        base_dir, "train_{}".format(ts)), sess.graph)
-    test_writer = tf.summary.FileWriter(os.path.join(
-        base_dir, "test_{}".format(ts)), sess.graph)
-    with tf.name_scope("Basic"):
-        tf.summary.scalar("Mean_Diff", tf.sqrt(model.cost))
-    summary = tf.summary.merge_all()
-    return summary, train_writer, test_writer
-
-
-def validate(sess, model, summary, feed, bno, epoch):
-    global bst_saver, bst_score, bst_file, bst_ckpt
-    print('{} running on test set...'.format(strftime("%H:%M:%S")))
-    mse, worst, test_summary_str = sess.run(
-        [model.cost, model.worst, summary], feed)
-    diff, max_diff, predict, actual = math.sqrt(
-        mse), worst[0], worst[1], worst[2]
-    print('{} Epoch {} diff {:3.5f} max_diff {:3.4f} predict {} actual {}'.format(
-        strftime("%H:%M:%S"), epoch, diff, max_diff, predict, actual))
-    found_better = False
-    if diff < bst_score:
-        bst_score = diff
-        bst_file.seek(0)
-        bst_file.truncate()
-        bst_file.write('{}\n{}\n'.format(diff, bno))
-        bst_file.truncate()
-        bst_saver.save(sess, bst_ckpt,
-                       global_step=tf.train.get_global_step())
-        print('{} acquired better model with validation score {}, at batch {}'.format(
-              strftime("%H:%M:%S"), diff, bno))
-        found_better = True
-    return test_summary_str, found_better
-
-
-def getInput(start, args):
-    ds = args.ds.lower()
-    print('{} using data source: {}'.format(strftime("%H:%M:%S"), args.ds))
-    if ds == 'db':
-        return input_fn.getInputs(
-            start, TIME_SHIFT, feat_cols, MAX_STEP, args.parallel,
-            args.prefetch, args.db_pool, args.db_host, args.db_port, args.db_pwd, args.vset or VSET)
-    elif ds == 'bigquery':
-        return input_bq.getInputs(start, TIME_SHIFT, feat_cols, MAX_STEP, TEST_BATCH_SIZE,
-                                  vset=args.vset or VSET)
-    elif ds == 'file':
-        return input_file.getInputs(
-            args.dir, start, args.prefetch, args.vset or VSET)
-    return None
 
 
 def run(args):
@@ -121,12 +39,13 @@ def run(args):
     tf.logging.set_verbosity(tf.logging.INFO)
     random.seed(SEED)
     keep_prob = tf.placeholder(tf.float32, [], name="keep_prob")
+    learning_rate = tf.placeholder(tf.float32, [], name="learning_rate")
     with tf.Session() as sess:
-        model = drnn.DRnnRegressorV5(
+        model = drnn.DRnnRegressorV6(
             dim=DIM,
             keep_prob=keep_prob,
             layer_width=LAYER_WIDTH,
-            learning_rate=LEARNING_RATE)
+            learning_rate=learning_rate)
         model_name = model.getName()
         print('{} using model: {}'.format(strftime("%H:%M:%S"), model_name))
         f = __file__
@@ -195,19 +114,28 @@ def run(args):
         summary, train_writer, test_writer = collect_summary(
             sess, model, training_dir)
         test_summary_str = None
+        decayed_lr = False
         while True:
             # bno = epoch*TEST_INTERVAL
             epoch = bno // TEST_INTERVAL
+            found_better = False
             if restored or bno % TEST_INTERVAL == 0:
-                test_summary_str = validate(
-                    sess, model, summary, {d['handle']: test_handle, keep_prob: 1}, bno, epoch)
+                test_summary_str, found_better = validate(
+                    sess, model, summary, {d['handle']: test_handle, keep_prob: 1, learning_rate: LEARNING_RATE}, bno, epoch)
                 restored = False
             try:
-                kp = min(1, random.uniform(KEEP_PROB, 1.25))
-                print('{} training batch {}, random keep_prob:{}'.format(
-                    strftime("%H:%M:%S"), bno+1, kp))
+                kp = min(1, random.uniform(KEEP_PROB, 1.05))
+                lr = LEARNING_RATE
+                if bno > int(DECAYED_LR_START * bno):
+                    if decayed_lr:
+                        lr = DECAYED_LEARNING_RATE
+                    elif found_better:
+                        decayed_lr = True
+                        lr = DECAYED_LEARNING_RATE
+                print('{} training batch {}, random keep_prob:{}, learning_rate:{}'.format(
+                    strftime("%H:%M:%S"), bno+1, kp, lr))
                 summary_str, worst = sess.run(
-                    [summary, model.worst, model.optimize], {d['handle']: train_handle, keep_prob: kp})[:-1]
+                    [summary, model.worst, model.optimize], {d['handle']: train_handle, keep_prob: kp, learning_rate: lr})[:-1]
             except tf.errors.OutOfRangeError:
                 print("End of Dataset.")
                 break
@@ -223,8 +151,8 @@ def run(args):
                 saver.save(sess, checkpoint_file,
                            global_step=tf.train.get_global_step())
         # test last epoch
-        test_summary_str = validate(
-            sess, model, summary, {d['handle']: test_handle, keep_prob: 1}, bno, epoch)
+        test_summary_str, _ = validate(
+            sess, model, summary, {d['handle']: test_handle, keep_prob: 1, learning_rate: LEARNING_RATE}, bno, epoch)
         train_writer.add_summary(summary_str, bno)
         test_writer.add_summary(test_summary_str, bno)
         train_writer.flush()
