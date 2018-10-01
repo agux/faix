195502872c50edeb80197feec062dfc1760383ef
Jonathan <carusyte@163.com>
2018-10-01 23:52:27 +0800

    upload nohup.log before shutting down

--- corl/wcc/command.sh
+++ corl/wcc/command.sh
@@ -4,10 +4,13 @@ python wcc_infer.py --prefetch=5 -m [model destination] -r gs://carusytes_bucket
 if [ $? -eq 0 ]; then
     sleep 15
     echo "job complete, shutting down vm." | mail -s "Job Complete" 3110798@qq.com
-    echo 'syncing file system...'
-    sync
-    echo 'shutting down vm...'
-    sudo shutdown -h now
 else
     echo FAIL
 fi
+
+echo 'syncing file system...'
+sync
+tar -xvf nohup.tar.gz nohup.log
+gsutil cp "nohup.tar.gz" gs://carusytes_bucket/
+echo 'shutting down vm...'
+sudo shutdown -h now
