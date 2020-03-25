4fae0da93fdb329aea0fa9248cd0ec083ae2c3e0
Jonathan <carusyte@163.com>
2020-03-25 21:44:15 +0800

    add requirements.txt

--- corl/wc_test/command.sh
+++ corl/wc_test/command.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-python test16.py --prefetch=2 --ds=db --db_pool=4 --db_host=['replace'] --db_port=['replace'] --db_pwd=['replace']
+python3 test16.py --prefetch=2 --ds=db --db_pool=4 --db_host=['replace'] --db_port=['replace'] --db_pwd=['replace']
 if [ $? -eq 0 ]; then
     sleep 15
     #echo "Training complete, shutting down vm." | mail -s "Training Complete" 3110798@qq.com
