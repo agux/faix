b3ffa8a4866bd00d73f4d8b4785a12ffa9e23059
Jonathan <carusyte@163.com>
2020-03-31 14:00:35 +0800

    support python 3.6 for asyncio

--- corl/wc_test/test16.py
+++ corl/wc_test/test16.py
@@ -222,4 +222,9 @@ async def main():
 if __name__ == '__main__':
     log.setLevel(logging.DEBUG)
     args = parseArgs()
-    asyncio.run(main())
+
+    # asyncio.run is new in Python 3.7 only
+    #asyncio.run(main())
+
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(main())
