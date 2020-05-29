8801bc02952aefa8522209741908ef1545b4cc23
Jonathan <carusyte@163.com>
2020-05-29 15:13:32 +0800

    fiddling with DelayedCosineDecayRestarts

--- corl/model/tf2/common.py
+++ corl/model/tf2/common.py
@@ -6,19 +6,39 @@ from time import strftime
 
 class DelayedCosineDecayRestarts(keras.optimizers.schedules.LearningRateSchedule):
 
-    def __init__(self, decay_start, *args, **kwargs):
-        super(DelayedCosineDecayRestarts, self).__init__(**kwargs)
-        self._decay_start = decay_start
-        self.cdr = keras.experimental.CosineDecayRestarts(*args, **kwargs)
+    def __init__(self, initial_learning_rate, decay_start, first_decay_steps, t_mul=2.0, m_mul=1.0, alpha=0.0,
+        name=None, *args, **kwargs):
+
+        super(DelayedCosineDecayRestarts, self).__init__()
+        self.initial_learning_rate=initial_learning_rate
+        self.decay_start = decay_start
+        self.first_decay_steps = first_decay_steps
+        self.t_mul = t_mul
+        self.m_mul = m_mul
+        self.alpha = alpha
+        self.name = name
+        self.cdr = keras.experimental.CosineDecayRestarts(
+            initial_learning_rate, 
+            first_decay_steps, 
+            t_mul, 
+            m_mul, 
+            alpha,
+            name+"_cdr")
 
     def __call__(self, step):
         return tf.cond(
-            tf.less(step, self._decay_start),
+            tf.less(step, self.decay_start),
             self.initial_learning_rate,
-            self.cdr(step-self._decay_start+1)
+            self.cdr(step-self.decay_start+1)
         )
     
     def get_config(self):
         return {
-            "decay_start": self._decay_start
+            "initial_learning_rate": initial_learning_rate,
+            "decay_start": self.decay_start,
+            "first_decay_steps": first_decay_steps,
+            "t_mul": t_mul,
+            "m_mul": m_mul,
+            "alpha": alpha,
+            "name": name
         }
