35e1d9ed1a246dbbe2ddc038716c7624365fdbf2
Jonathan <carusyte@163.com>
2020-05-29 15:27:07 +0800

    use lambda function in tf.cond

--- corl/model/tf2/common.py
+++ corl/model/tf2/common.py
@@ -4,45 +4,20 @@ import tensorflow as tf
 from tensorflow import keras
 from time import strftime
 
-class DelayedCosineDecayRestarts(keras.optimizers.schedules.LearningRateSchedule):
+class DelayedCosineDecayRestarts(keras.experimental.CosineDecayRestarts):
 
-    def __init__(self, initial_learning_rate, decay_start, first_decay_steps, t_mul=2.0, m_mul=1.0, alpha=0.0,
-        name=None, *args, **kwargs):
-
-        super(DelayedCosineDecayRestarts, self).__init__()
-        self.initial_learning_rate=initial_learning_rate
-        self.decay_start = decay_start
-        self.first_decay_steps = first_decay_steps
-        self.t_mul = t_mul
-        self.m_mul = m_mul
-        self.alpha = alpha
-        self.name = name or  "DelayedCosineDecayRestarts"
-        self.cdr = keras.experimental.CosineDecayRestarts(
-            initial_learning_rate, 
-            first_decay_steps, 
-            t_mul, 
-            m_mul, 
-            alpha,
-            self.name+"_cdr")
+    def __init__(self, decay_start, *args, **kwargs):
+        super(DelayedCosineDecayRestarts, self).__init__(*args, **kwargs)
+        self._decay_start = decay_start
 
     def __call__(self, step):
-        # return tf.cond(
-        #     tf.less(step, self.decay_start),
-        #     self.initial_learning_rate,
-        #     self.cdr(step-self.decay_start+1)
-        # )
-        return self.initial_learning_rate \
-            if step < self.decay_start \
-            else self.cdr(step-self.decay_start+1)
+        return tf.cond(
+            tf.less(step, self._decay_start), 
+            lambda: self.initial_learning_rate,
+            lambda: super(DelayedCosineDecayRestarts, self).__call__(step-self._decay_start+1)
+        )
 
-    
     def get_config(self):
         return {
-            "initial_learning_rate": initial_learning_rate,
-            "decay_start": self.decay_start,
-            "first_decay_steps": first_decay_steps,
-            "t_mul": t_mul,
-            "m_mul": m_mul,
-            "alpha": alpha,
-            "name": name
+            "decay_start": self._decay_start
         }
