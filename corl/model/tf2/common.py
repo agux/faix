c939bf119f4dbfc6fec1e1d2c3f5f7572f827d41
Jonathan <carusyte@163.com>
2020-05-29 15:00:08 +0800

    fiddling with DelayedCosineDecayRestarts

--- corl/model/tf2/common.py
+++ corl/model/tf2/common.py
@@ -4,17 +4,18 @@ import tensorflow as tf
 from tensorflow import keras
 from time import strftime
 
-class DelayedCosineDecayRestarts(keras.experimental.CosineDecayRestarts):
+class DelayedCosineDecayRestarts(keras.optimizers.schedules.LearningRateSchedule):
 
     def __init__(self, decay_start, *args, **kwargs):
         super(DelayedCosineDecayRestarts, self).__init__(*args, **kwargs)
         self._decay_start = decay_start
+        self.cdr = keras.experimental.CosineDecayRestarts(*args, **kwargs)
 
     def __call__(self, step):
         return tf.cond(
-            tf.less(step, self._decay_start), 
+            tf.less(step, self._decay_start),
             self.initial_learning_rate,
-            super(DelayedCosineDecayRestarts, self).call(step-self._decay_start+1)
+            self.cdr(step-self._decay_start+1)
         )
     
     def get_config(self):
