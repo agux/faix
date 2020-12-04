c4601542031203d6e4ff3d036224cf8561bf9cfd
Jonathan <carusyte@163.com>
2020-12-04 18:27:20 +0800

    fix "TypeError: missing a required argument: 'host'"

--- corl/wcc/worker.py
+++ corl/wcc/worker.py
@@ -256,7 +256,7 @@ def _load_data(work, num_actors, min_rcode, shared_args, shared_args_oid, data_q
     #     _init(1, db_host, db_port, db_pwd)
 
     actor_pool = ray.util.ActorPool(
-        [DataLoader.remote() for _ in range(num_actors)]
+        [DataLoader.remote(host='127.0.0.1') for _ in range(num_actors)]
     )
 
     # poll work request from 'work_queue' for data loading, and push to 'data_queue'
